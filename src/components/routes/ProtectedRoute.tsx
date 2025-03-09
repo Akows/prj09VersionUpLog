2312dfc939afc5a36b9b9e3e92e51a99926555a9
이유승 <akows141@gmail.com>
2025-03-09 12:33:55 +0900

    Fix: 로그인 상태에서 새 글 작성 페이지에 접근이 불가능한 문제를 수정. - 로그인 상태 검증 작업이 아직 완료되지 않은 상태에서, ProtectedRoute가 사용자 정보가 없다고 간주하는 것이 원인. - Loading 상태를 받아와서 작업이 종료되기 전까지 잠시 대기하도록 조치함.

--- src/components/routes/ProtectedRoute.tsx
+++ src/components/routes/ProtectedRoute.tsx
@@ -7,9 +7,14 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
   const location = useLocation();
 
+  // 로딩 중이면 잠시 로딩 UI 표시
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+    
   // 로그인 하지 않은 사용자는 Home 페이지로 리다이렉트
   if (!user) {
     return <Navigate to="/" state={{ from: location }} replace />;
